e795dda41c65bd4746e8788d9d76c3b55956ad5c
CapitaineToinon <antoinesutter@gmail.com>
2023-12-27 19:46:43 +0100

    Trying fix for keystatic redirects

---
+++ src/pages/api/keystatic/github/login.ts
@@ -0,0 +1,52 @@
+/**
+ * This whole thing is a work around to fix the following issue:
+ * https://github.com/Thinkmill/keystatic/issues/835
+ * 
+ * TODO remove when fixed by keystatic
+ */
+import type { APIRoute } from 'astro'
+import { createRegExp, word, char, oneOrMore } from 'magic-regexp'
+import { all } from '@/pages/api/keystatic/[...params]'
+import keystaticConfig from 'keystatic.config'
+
+const regex = createRegExp(
+	'branch/',
+	word.as('branch'),
+	'/collection/',
+	word.as('collection'),
+	'/item/',
+	oneOrMore(char).as('slug'),
+)
+
+export const prerender = false
+
+export const GET: APIRoute = (params) => {
+	if (keystaticConfig.storage.kind !== 'github') {
+		return all(params)
+	}
+
+	const url = new URL(params.request.url)
+	const from = url.searchParams.get('from')
+
+	if (!from) {
+		return all(params)
+	}
+
+	const matches = regex.exec(from)
+
+	if (!matches || !matches.groups) {
+		return all(params)
+	}
+
+	const { branch, collection, slug } = matches.groups
+	const fixed = `branch/${branch}/collection/${collection}/item/${encodeURIComponent(
+		slug,
+	)}`
+
+	url.searchParams.set('from', fixed)
+
+	return all({
+		...params,
+		request: new Request(url, params.request),
+	})
+}
