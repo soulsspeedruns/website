f44cf68e11d768a4c60a68b2fdd3d48d2128a80d
CapitaineToinon <capitainetoinon@proton.me>
2025-09-23 21:07:41 +0200

    Upgraded everything (#143)
    * Add SCM text on main page
    
    * Fixed homepage
    
    * removed duplicates
    
    * WHEN DID THIS SWITCH TO TRUE
    
    * added analytics
    
    * replaced youtube embed
    
    * Updated to node 24
    
    * Trying to fix shit
    
    ---------
    
    Co-authored-by: Kahmul <kahmul78@web.de>
    Co-authored-by: CapitaineToinon <antoinesutter@gmail.com>

--- src/components/markdoc/youtube.preview.tsx
+++ src/components/markdoc/youtube.preview.tsx
@@ -1,18 +1,18 @@
-import getYouTubeId from 'get-youtube-id'
-import LiteYouTubeEmbed from 'react-lite-youtube-embed'
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
-
 export type Props = {
 	src: string
 }
 
 export function YouTube({ src }: Props) {
-	const id = getYouTubeId(src) ?? src
-
 	return (
-		<LiteYouTubeEmbed
-			id={id}
-			title="Youtube Video"
-		/>
+		<iframe
+			width="560"
+			height="315"
+			src={src}
+			title="YouTube video player"
+			frameBorder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerPolicy="strict-origin-when-cross-origin"
+			allowFullScreen
+		></iframe>
 	)
 }
