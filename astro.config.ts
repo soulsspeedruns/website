4b2674be3d3d8aed3023a9879ded0d40a98c29da
CapitaineToinon <capitainetoinon@proton.me>
2025-09-23 20:54:29 +0200

    Trying to fix shit

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,24 @@
-import { defineConfig, type ViteUserConfig } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import markdoc from '@astrojs/markdoc'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 import prefetch from '@astrojs/prefetch'
 import sitemap from '@astrojs/sitemap'
 import { MagicRegExpTransformPlugin } from 'magic-regexp/transform'
 
-const vite: ViteUserConfig = {
-	server: {
-		fs: {
-			allow: ['../'],
-		},
-	},
-	plugins: [MagicRegExpTransformPlugin.vite()],
-}
-
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
+	output: 'static',
 	adapter: vercel(),
-	vite,
+	vite: {
+		server: {
+			fs: {
+				allow: ['../'],
+			},
+		},
+		plugins: [MagicRegExpTransformPlugin.vite()],
+	},
 	site: 'https://soulsspeedruns.com',
 	integrations: [
 		markdoc(),
